13ab302edf0c5f29cf9e82c9f3d70ea880b4405b
biacosta7 <contact.beatrizcosta@gmail.com>
2024-01-31 21:14:51 -0300

    continuacao

--- src/app.js
+++ src/app.js
@@ -21,11 +21,46 @@ app.get("/clientes", (req, res) => {
     res.status(200).json(clientes);
 })
 
+//Consultar o cliente pelo id
+app.get("/clientes/:id", (req, res) => {
+    const index = buscarCliente(req.params.id); // (req.params.id) traz o parametro id na requisicao
+    res.status(200).json(clientes[index]) // clientes é array, index recebeu o retorno da busca
+})
+
 //Cadastrar novo cliente
 app.post("/clientes", (req, res) => {
     clientes.push(req.body);  //adiciona na ultima posicao
     res.status(201).send("Cliente cadastrado com sucesso!") //status 201 indica confirmacao que foi criado com sucesso
 })
+
+//Atualizar o cadastro de um cliente
+app.put("/clientes/:id", (req,res) => {
+    const index = buscarCliente(req.params.id);
+    clientes[index].nome = req.body.nome; //acessar o nome na requisacao na parte "body"
+    res.status(200).json(clientes) //exibe todos clientes
+
+})
+//nome atualizar, no PUT > body (json) > {"nome": "novo nome"}
+
+
+//Excluir um cliente
+app.delete("/clientes/:id", (req,res) => {
+    const index = buscarCliente(req.params.id);
+    clientes.splice(index, 1); //excluir posicao e quantas casas
+    res.status(200).send("Cadastro removido com sucesso!") 
+
+})
+
+function buscarCliente(id){
+    var index = clientes.findIndex(cliente => cliente.id === Number(id)) //number(id) converte o id em número
+    return index
+}
+
+// function buscarCliente(id){
+//     clientes.findIndex(parametro => {cliente.id === i}}) faz o loop no id do cliente ate o id do cliente for igual ao id desejado 
+// } 
+//find = retorna valor e findIndex = retorna a posicão
+
 
 
 export default app;
