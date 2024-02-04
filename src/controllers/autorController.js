4be9c1c6e37f676b522acd4cd6f456533c8f8191
José Pedro <jose50244966@edu.pe.senac.br>
2024-02-03 22:36:02 -0300

    Concluido todos os controladores de autor

--- src/controllers/autorController.js
+++ src/controllers/autorController.js
@@ -1,37 +1,83 @@
+import autor from "../models/autorModel.js"
+
 
 // Lista de todos os autores
 // Rota /autor
 // Método GET
 const listaDeAutores = async(req, res) => {
-    res.send(`<h1>Página com todos os autores </h1>`)
+    const listaAutores = await autor.find({});
+    if(listaAutores){
+        res.status(200).json(listaAutores);
+    } else if(listaAutores === null){
+        res.status(404).json({menssagem: "Nenhum autor foi encontrado"});
+    }
 };
 
 // Registrar novos autores
 // Rota /autor
 // Método POST
 const registrarAutor = async(req, res) => {
-    res.json({menssagem: "Autor registrado com sucesso"})
+    if(!req.body.nome){
+        return res.status(400).json({menssagem: "Insira o nome do autor"});
+    }
+    const novoAutor = await autor.create(req.body);
+    if(novoAutor){
+        return res.status(201).json(novoAutor);
+    }
 };
 
 // Informação do autor por id
 // Rota /autor/:id
 // Método GET
 const autorPorId = async(req, res) => {
-    res.json({menssagem: "Informações do autor de id ..."})
+    let id = req.params.id
+    if(!id){
+        return res.status(400).json({menssagem: "Nenhum id foi passado"})
+    }
+    const findAutor = await autor.findById(id);
+    if(findAutor){
+        return res.status(200).json(findAutor);
+    } else if(findAutor === null){
+        return res.status(404).json({menssagem: `Não foi encontrado nenhum ator com id ${id}`});
+    }
 };
 
 // Atualizar autor por id
 // Rota /autor/:id
 // Método PUT
 const atualizarAutor = async(req, res) => {
-    res.json({menssagem: "Autor atualizado com sucesso"})
+    let id = req.params.id;
+    if(!id){
+        return res.status(400).json({menssagem: "Nenhum id foi passado"});
+    }
+    const updateAutor = req.body;
+    if(!updateAutor.nome && !updateAutor.nascimento && !updateAutor.falecimento){
+        return res.status(400).json({menssagem: "Você precisa inserir informoções para atualizar"});
+    }
+    const autorAtualizado = await autor.findByIdAndUpdate(id, updateAutor, {new: true});
+    if(autorAtualizado){
+        return res.status(200).json(autorAtualizado);
+    } else if(autorAtualizado === null){
+        return res.status(404).json({menssagem: `Não foi encontrado nenhum ator com id ${id}`});
+    }
 };
 
 // Deletar autor por id
 // Rota /autor/:id
-// Método Delete
+// Método Delete   
+/*OBS: PRECISA REMOVER TODOS OS LIVROS DO AUTOR*/
 const apagarAutor = async(req, res) => {
-    res.json({menssagem: "Autor de id ... foi deletado"})
+    let id = req.params.id
+    if(!id){
+        return res.status(400).json({menssagem: "Nenhum id foi passado"})
+    }
+    const deleteAutor = await autor.findByIdAndDelete(id);
+    if(deleteAutor){
+        return res.status(200).json({menssagem: `Autor de id ${id} foi deletado`});
+    } else if(deleteAutor === null){
+        return res.status(404).json({menssagem: `Não foi encontrado nenhum ator com id ${id}`})
+    }
+    
 };
 
 export {
